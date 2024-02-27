0f7005b56bf953c6f78d1e324e0dcd7a111c9585
Victor Ortega <51211089+vorteg@users.noreply.github.com>
2024-02-27 21:10:13 +0000

    Binary Search Full Script

--- 1 binary/binary_search.py
+++ 1 binary/binary_search.py
@@ -1,37 +1,37 @@
 from typing import Tuple
+import sys
 
 def boot_index(data:list[str])-> Tuple[int]:
     lower = 0
     upper = len(data) - 1
     return lower, upper
 
-def calculate_middle_index(lower:int, upper:int) -> int:
-    sum_index = lower + upper
-    if sum_index == 1:
-        return lower
-    return sum_index // 2
+def calculate_middle_index(lower:int, upper:int) -> int:      
+    return (lower + upper) // 2
 
-def get_index(md_index:str,search_element:str) -> Tuple[bool]:
+def get_index(md_index:str,search_element:str) -> bool:
     if md_index == search_element:
         return True
+    return False
 
-def greater_or_less(md_index:int,lower_index:int,upper_index:int)->Tuple[int]:
-    if md_index > upper_index:
-        lower_index = upper_index
-        return 
+def greater_or_less(data:list[str],md_index:int,lower_index:int,upper_index:int,search_element:str)->Tuple[int]:
+    if data[md_index] > search_element:
+        upper_index = md_index  - 1  
+    else:
+        lower_index = md_index + 1
+    return lower_index,upper_index
 
 def main(data:list[str]) -> None:
-    search_element = input("ingresa el nombre que desas buscar").lower()
+    search_element = input("\n ingresa el nombre que desas buscar \n").lower()
     data = sorted([name.lower() for name in data])
     lower_index, upper_index = boot_index(data)
-    md_index = calculate_middle_index(lower_index,upper_index)
-    while md_index > 0:
+    while upper_index - lower_index >= 0:
         md_index = calculate_middle_index(lower_index,upper_index)
         finded = get_index(data[md_index],search_element)
         if finded :
             print(f"EL indice para {data[md_index]} es {md_index}")
-            break
-        lower_index,upper_index = greater_or_less(md_index,lower_index,upper_index)
+            sys.exit()
+        lower_index,upper_index = greater_or_less(data,md_index,lower_index,upper_index,search_element)
     print("No se encontraron datos :(")
 
 
